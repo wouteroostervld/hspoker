96f4776ba825d37fac8f1cb54abb6f19a8e855a3
Wouter Oosterveld <wouter@fizzyflux.nl>
2014-03-10 20:04:59 +0100

    Initial commit

---
+++ Pokerhand.hs
@@ -0,0 +1,34 @@
+module Pokerhand where
+
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace  deriving (Enum, Eq, Ord, Show)
+data Suit = Clubs | Hearts | Diamonds | Spades deriving (Enum,Eq,Show)
+data Card = Card Rank Suit deriving (Show)
+rank (Card r _) = r
+suit (Card _ s) = s
+cards = [ Card r s | r <- [Two .. ], s <- [ Clubs .. ] ]
+
+showSuit Clubs = "c"
+showSuit Diamonds = "d"
+showSuit Spades = "s"
+showSuit Hearts = "h"
+
+showRank Two = "2"
+showRank Three = "3"
+showRank Four = "4"
+showRank Five = "5"
+showRank Six = "6"
+showRank Seven = "7"
+showRank Eight = "8"
+showRank Nine = "9"
+showRank Ten = "T"
+showRank Jack = "J"
+showRank Queen = "Q"
+showRank King = "K"
+showRank Ace = "A"
+
+showCard (Card r s ) = showRank r ++ showSuit s
+
+suitFromString "s" = Spades
+suitFromString "c" = Clubs
+suitFromString "d" = Diamonds
+suitFromString "h" = Hearts
---
+++ test/SpecPokerhand.hs
@@ -0,0 +1,47 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import Pokerhand
+
+main :: IO()
+main = hspec $ do
+    describe "cards" $ do
+        it "has length 52" $
+            length cards `shouldBe` 52
+
+    describe "rank" $ do
+        it "returns Ace for Card Ace Clubs" $ do
+            rank ( Card Ace Clubs ) `shouldBe` Ace
+
+    describe "showRank" $ do
+        it "shows the right string for every rank" $ do
+            map (showRank) [ Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace ] `shouldBe`
+                [ "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A" ] 
+
+    describe "suit" $ do
+        it "returns Club for Card Ace Clubs" $ do
+            suit ( Card Ace Clubs ) `shouldBe` Clubs
+
+    describe "Suit" $ do
+        it "shows the right string for every suit" $ do
+            map (showSuit) [ Clubs, Diamonds, Spades, Hearts ] `shouldBe` [ "c", "d", "s", "h" ]
+
+    describe "Card" $ do
+        it "shows Ac for Card Ace Clubs" $ do
+            showCard ( Card Ace Clubs ) `shouldBe` "Ac"
+
+        it "shows Kc for Card King Clubs" $ do
+            showCard ( Card King Clubs ) `shouldBe` "Kc"
+
+        it "shows Kd for Card King Diamonds" $ do
+            showCard ( Card King Diamonds ) `shouldBe` "Kd"
+
+        it "showCard 2s for Card Two Spades" $ do
+            showCard ( Card Two Spades ) `shouldBe` "2s"
+
+        it "shows (showRank r ++ showSuit s ) for arbitrary Card r s" $ do
+            [ showRank r ++ showSuit s | r <- [ Two .. ], s <- [ Clubs .. ] ] `shouldBe` [ showCard ( Card r s )| r <- [ Two .. ], s <- [ Clubs .. ] ]
+
+    describe "suitFromString" $ do
+        it "should return the right type" $ do
+            map (suitFromString) [ "c", "d", "h", "s" ] `shouldBe` [ Clubs, Diamonds, Hearts, Spades ] 
