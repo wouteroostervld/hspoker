4bd8f80cd8ade0fb7e84595410cb471c4716a79d
Wouter Oosterveld <wouter@fizzyflux.nl>
2014-03-19 22:26:33 +0100

    Refactoring

--- src/HSPoker/Hand.hs
+++ src/HSPoker/Hand.hs
@@ -8,8 +8,12 @@ handFromString s | length cards == 2 = Just cards
 
 allhands = [ [c1,c2] | c1 <- cards, c2 <- cards, c1 > c2 ]
 
-handHasRanks r1 r2 h = (rank (h!!0) == r1) && (rank (h!!1) == r2) 
-handsFromRange (x:y:xs) | test = Just $ filter (handHasRanks (fromJust rank1) (fromJust rank2)) allhands
+filterOnRank i r l = filter (\h -> rank (h!!i) == r ) l
+
+filterOnRankFirst r l = filterOnRank 0 r l
+filterOnRankSecond r l = filterOnRank 1 r l
+
+handsFromRange (x:y:xs) | test = Just $ filterOnRankSecond (fromJust rank2) $ filterOnRankFirst (fromJust rank1) allhands
                         | otherwise = Nothing
                 where   rank1 = rankFromChar x
                         rank2 = rankFromChar y
